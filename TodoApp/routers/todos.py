d7f9bb982af437588041071f63081326cad0a787
michaelblossom <michaelblossom8654@gmail.com>
2026-02-10 14:20:17 -0800

    Fix typo in example description of TodoRequest payload

--- TodoApp/routers/todos.py
+++ TodoApp/routers/todos.py
@@ -35,7 +35,7 @@ class TodoRequest(BaseModel):
             "examples": [
                 {
                     "title": "List of provisions to buy",
-                    "description": "Milk, Tea, bread,biscuit,corm-flakes",
+                    "description": "Milk, Tea, bread,biscuit,corn-flakes",
                     "priority": 3,
                     "duration": 3,
                     "completed": False

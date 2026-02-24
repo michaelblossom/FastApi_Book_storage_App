0ec80fc59770cab8b7a7fb254840c0a95e3d80a7
michaelblossom <michaelblossom8654@gmail.com>
2026-02-24 09:32:32 -0800

    Update example description, priority, and duration in TodoRequest payload

--- TodoApp/routers/todos.py
+++ TodoApp/routers/todos.py
@@ -35,9 +35,9 @@ class TodoRequest(BaseModel):
             "examples": [
                 {
                     "title": "List of provisions to buy",
-                    "description": "Milk, Tea, bread,biscuit,corn-flakes,chocolate,shawarama",
-                    "priority": 4,
-                    "duration": 6,
+                    "description": "Milk, Tea, bread,biscuit,corn-flakes,chocolate,sugar",
+                    "priority": 3,
+                    "duration": 5,
                     "completed": False
                 }
             ]

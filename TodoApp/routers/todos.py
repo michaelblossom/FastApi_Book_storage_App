da20871e217c2680a4696053413bce6a5e51af9b
michaelblossom <michaelblossom8654@gmail.com>
2026-03-06 11:03:48 -0800

    Fix typo in TodoRequest example description

--- TodoApp/routers/todos.py
+++ TodoApp/routers/todos.py
@@ -35,7 +35,7 @@ class TodoRequest(BaseModel):
             "examples": [
                 {
                     "title": "List of provisions to buy",
-                    "description": "Milk, Tea, bread,biscuit,corn-flakes,chocolate,sugar,cakes,ice-cream",
+                    "description": "Milk, Tea, bread,biscuit,corn-flakes,chocolate,sugar,cake",
                     "priority": 4,
                     "duration": 4,
                     "completed": False

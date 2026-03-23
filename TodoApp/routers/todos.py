1fac79ebcc07bd1e8517bcccaa28cd25ab05d2d4
michaelblossom <michaelblossom8654@gmail.com>
2026-03-23 12:43:15 -0700

    Update example description in TodoRequest to remove unnecessary item

--- TodoApp/routers/todos.py
+++ TodoApp/routers/todos.py
@@ -35,7 +35,7 @@ class TodoRequest(BaseModel):
             "examples": [
                 {
                     "title": "List of provisions to buy",
-                    "description": "Milk, Tea, bread,biscuit,corn-flakes,chocolate,sugar,soap,detergent",
+                    "description": "Milk, Tea, bread,biscuit,corn-flakes,chocolate,sugar,soap",
                     "priority": 5,
                     "duration": 5,
                     "completed": False

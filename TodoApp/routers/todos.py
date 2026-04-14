9ff901087e3313a508b6031b7da100e2d17b8488
michaelblossom <michaelblossom8654@gmail.com>
2026-04-14 14:09:04 -0700

    Update TodoRequest example priority and duration values

--- TodoApp/routers/todos.py
+++ TodoApp/routers/todos.py
@@ -36,8 +36,8 @@ class TodoRequest(BaseModel):
                 {
                     "title": "List of provisions to buy",
                     "description": "Milk,gesha,sadin,detergent,Tea,bread,Butter,biscuit,corn-flakes,chocolate,sugar,soap,gloceries,water,oat,fruits",
-                    "priority":4,
-                    "duration": 4,
+                    "priority":5,
+                    "duration": 5,
                     "completed": False
                 }
             ]

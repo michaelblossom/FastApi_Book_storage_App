c337cc8b6b47bd4e9fc4fec49937d45e34661676
michaelblossom <michaelblossom8654@gmail.com>
2026-04-28 12:18:41 -0700

    Update TodoRequest priority field constraints to allow values between 5 and 10

--- TodoApp/routers/todos.py
+++ TodoApp/routers/todos.py
@@ -26,7 +26,7 @@ db_dependency = Annotated[Session, Depends(get_db)]
 class TodoRequest(BaseModel):
     title: str = Field(min_length=3)
     description: str = Field(min_length=10, max_length=50)
-    priority: int = Field(ge=1, le=5)
+    priority: int = Field(ge=5, le=10)
     duration: int = Field(ge=1)
     completed: bool = False
 

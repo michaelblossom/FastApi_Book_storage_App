b2cbe34b0e1450feb3a65c4265d40a9f1da3fe91
michaelblossom <michaelblossom8654@gmail.com>
2026-06-03 05:19:44 -0700

    Update TodoRequest example with improved description clarity

--- TodoApp/routers/todos.py
+++ TodoApp/routers/todos.py
@@ -35,8 +35,7 @@ class TodoRequest(BaseModel):
             "examples": [
                 {
                     "title": "List of provisions to buy",
-                    "description":"Milk,"
-                    "detergent,Tea,bread,gesha,Butter,biscuit,corn-flakes,chocolate,sugar,soap,gloceries,water,oat",
+                    "description":"The things that need to be bought include detergent, Tea, bread, gesha, Butter, biscuit, corn-flakes, chocolate, sugar, soap, groceries, water, oat",
                     "priority":5,
                     "duration": 4,
                     "completed": False

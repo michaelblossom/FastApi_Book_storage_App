e05145da95979c263ec0e85911174dd7d67ead98
michaelblossom <michaelblossom8654@gmail.com>
2026-07-04 12:28:19 -0700

    update description field minimum length to 9 in TodoRequest model

--- TodoApp/routers/todos.py
+++ TodoApp/routers/todos.py
@@ -25,7 +25,7 @@ db_dependency = Annotated[Session, Depends(get_db)]
 
 class TodoRequest(BaseModel):
     title: str = Field(min_length=5, max_length=20)
-    description: str = Field(min_length=10, max_length=50)
+    description: str = Field(min_length=9, max_length=50)
     priority: int = Field(ge=4, le=9)
     duration: int = Field(ge=4)
     completed: bool = False

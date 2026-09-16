3f48b2ca5e2ed1365faad72fd331c01dbdd9706f
michaelblossom <michaelblossom8654@gmail.com>
2026-09-16 10:46:03 -0700

    fix priority example value in TodoRequest model to ensure valid range

--- TodoApp/routers/todos.py
+++ TodoApp/routers/todos.py
@@ -36,7 +36,7 @@ class TodoRequest(BaseModel):
                 {
                     "title": "List of provisions to buy",
                     "description":"The things that need to be bought include detergents, gesha, Butter, biscuit, corn-flakes, chocolate, sugar, soap, groceries, water, oat",
-                    "priority":5,
+                    "priority":3,
                     "duration": 4,
                     "completed": False
                 }
